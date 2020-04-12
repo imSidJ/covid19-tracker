c291c0d81d12fa2ea67f8ef441b173cef6125b80
imsidj <sid786india@gmail.com>
2020-04-12 14:55:32 +0530

    Implemented country picker and line chart

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
 
     state = {
         data: {},
+        country: '',
     }
 
     async componentDidMount() {
@@ -15,7 +16,16 @@ class App extends React.Component {
 
         this.setState({
             data: fetchedData,
-        })
+        });
+    }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        console.log(fetchedData);
+        this.setState({
+            data: fetchedData,
+            country: country,
+        });
     }
 
     render() {
@@ -23,7 +33,7 @@ class App extends React.Component {
         return (
             <div className={styles.container}>
                 <Cards data={data}/>
-                <CountryPicker />
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Chart />
             </div>
         )
